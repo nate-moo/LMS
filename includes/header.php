c3a1df7b0d0a13babddf28541973de7c8a271d53
nate-moo <40650681+nate-moo@users.noreply.github.com>
2018-12-14 16:56:17 -0500

    ...

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,28 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
   <body>
     <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #4c8ef7;>
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="/index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Courses</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Chat</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Profile</a>
+        </li>
+      </ul>
+    </nav>
